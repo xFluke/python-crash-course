e3f864cbaf906c9d476c5730fcdc5b99bf5fb0dd
xFluke <43255466+xFluke@users.noreply.github.com>
2018-09-14 17:03:51 -0400

    Completed project as shown in the book
    To do:
    - Refactor code
    - Add new features

--- Project 1/alien_invasion/alien_invasion.py
+++ Project 1/alien_invasion/alien_invasion.py
@@ -9,9 +9,10 @@ import pygame
 from pygame.sprite import Group
 
 import game_functions as gf
-
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 
 def run_game():
@@ -22,8 +23,12 @@ def run_game():
             (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Create an instance to store game statistics
+    # Make the Play button
+    play_button = Button(ai_settings, screen, "Play")
+
+    # Create an instance to store game statistics and create a Scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # Make a ship, a group of bullets, and a group of aliens
     ship = Ship(ai_settings, screen)
@@ -35,13 +40,18 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
+                aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
+                bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb,  ship, aliens,
+                bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
+                play_button)
+
 
 run_game()
--- Project 1/alien_invasion/ship.py
+++ Project 1/alien_invasion/ship.py
@@ -6,10 +6,12 @@ Created on Wed Sep 12 19:57:23 2018
 """
 
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position"""
+        super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
