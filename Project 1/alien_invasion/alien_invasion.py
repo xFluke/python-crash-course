62bb1811f548475a40a6efc175fce541fe43f405
xFluke <43255466+xFluke@users.noreply.github.com>
2018-09-14 12:18:19 -0400

    Accidentally put 2 different folders last time

--- Project 1/alien_invasion/alien_invasion.py
+++ Project 1/alien_invasion/alien_invasion.py
@@ -11,6 +11,7 @@ from pygame.sprite import Group
 import game_functions as gf
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 
 def run_game():
@@ -20,21 +21,27 @@ def run_game():
     screen = pygame.display.set_mode(
             (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    
+
+    # Create an instance to store game statistics
+    stats = GameStats(ai_settings)
+
     # Make a ship, a group of bullets, and a group of aliens
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    
+
     # Create the fleet of aliens
     gf.create_fleet(ai_settings, screen, ship, aliens)
-    
+
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, ship, aliens)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-        
+
 run_game()
